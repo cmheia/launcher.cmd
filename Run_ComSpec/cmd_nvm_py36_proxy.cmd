0d38089876c19c0e126b1208d2d030df3e9ea105
cmheia <cmheia@gmail.com>
2018-01-20 01:18:39 +0800

    -debug echo

--- Run_ComSpec/cmd_nvm_py36_proxy.cmd
+++ Run_ComSpec/cmd_nvm_py36_proxy.cmd
@@ -18,9 +18,6 @@ set target_arg=%1
 set target_attr=%~a1
 set target_type=%target_attr:~,1%
 if defined target_arg (
-  echo target_arg is %target_arg%
-  echo target_attr is %target_attr%
-  echo target_type is %target_type%
   if "%target_type%" equ "d" (
     echo target is dir
     set work_dir=%~f1
