0e7ffe0bca7998459f1ef24bdb34bdd16066124e
cmheia <cmheia@gmail.com>
2018-01-20 00:38:45 +0800

    last.append(this)

--- Run_ComSpec/cmd_node_py36_c.cmd
+++ Run_ComSpec/cmd_node_py36_c.cmd
@@ -15,13 +15,29 @@ call %~dp0..\config\cfg_vscode_path.cmd
 
 set PATH=%SYS_PATH%;%PYTHON_PATH%;%NODE_PATH%;%MSYS_PATH%\mingw64\bin
 
-set TARGET_DIR=%1
+set PYTHON_VERSION=
 
-if defined TARGET_DIR (
-  cd %TARGET_DIR%
-  echo start at %TARGET_DIR%
-) else (
-  echo start at %~dp0
+set work_dir=%~dp0
+set target_arg=%1
+set target_attr=%~a1
+set target_type=%target_attr:~,1%
+if defined target_arg (
+  echo target_arg is %target_arg%
+  echo target_attr is %target_attr%
+  echo target_type is %target_type%
+  if "%target_type%" equ "d" (
+    echo target is dir
+    set work_dir=%~f1
+  ) else (
+    echo target is file
+    set work_dir=%~dp1
+  )
 )
+echo cd to "%work_dir%"
+cd "%work_dir%"
+set work_dir=
+set target_arg=
+set target_attr=
+set target_type=
 
 start %CONTITLE% %ComSpec%
--- Run_ComSpec/cmd_py27.cmd
+++ Run_ComSpec/cmd_py27.cmd
@@ -11,13 +11,29 @@ call %~dp0..\config\cfg_python%PYTHON_VER%_path.cmd
 
 set PATH=%SYS_PATH%;%PYTHON_PATH%
 
-set TARGET_DIR="%1"
+set PYTHON_VERSION=
 
-if exist %TARGET_DIR% (
-	echo cd to %TARGET_DIR%
-	cd %TARGET_DIR%
-) else (
-	echo run in %~dp0
+set work_dir=%~dp0
+set target_arg=%1
+set target_attr=%~a1
+set target_type=%target_attr:~,1%
+if defined target_arg (
+  echo target_arg is %target_arg%
+  echo target_attr is %target_attr%
+  echo target_type is %target_type%
+  if "%target_type%" equ "d" (
+    echo target is dir
+    set work_dir=%~f1
+  ) else (
+    echo target is file
+    set work_dir=%~dp1
+  )
 )
+echo cd to "%work_dir%"
+cd "%work_dir%"
+set work_dir=
+set target_arg=
+set target_attr=
+set target_type=
 
 start %CMD_TITLE% %ComSpec%
