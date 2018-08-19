0b921fabb04dcb29ba20993351fb1435242979cd
cmheia <cmheia@gmail.com>
2018-08-19 16:33:20 +0800

    node: update install log

--- config/install/node_install_modules.cmd
+++ config/install/node_install_modules.cmd
@@ -1,9 +1,14 @@
 @echo off
 
-call %~dp0set_node_proxy.cmd
+call %~dp0..\set_node_proxy.cmd
+call %~dp0..\cfg_sys_path.cmd
+call %~dp0..\cfg_node_path.cmd
+
+set PATH=%SYS_PATH%;%NODE_PATH%
 
 npm i -g cnpm --registry=https://registry.npm.taobao.org
-npm i -g uglifycss
+rem npm i -g clean-css
+rem npm i -g html-minifier
+rem npm i -g uglifycss
 npm i -g uglify-js
-npm i -g clean-css
-npm i -g html-minifier
+npm i -g csso-cli
